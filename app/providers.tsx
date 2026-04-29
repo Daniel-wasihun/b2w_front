84063d90b9885125441cd07ad8c6a21d11ee9cfb
Daniel Wasihun <daniwzgeta@gmail.com>
2026-04-29 17:55:55 +0300

    feat(ui): define global styles and providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -15,7 +15,12 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }, [fetchTranslations, fetchUser]);
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem 
+      disableTransitionOnChange
+    >
       {children}
     </ThemeProvider>
   );
